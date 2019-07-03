d014a7dbdde7e8d1162b6ed640485c0204b84980
Bubo Howler <49031316+rarestowl@users.noreply.github.com>
2019-07-03 08:41:46 +0530

    Update BST_construction.cpp

--- BST_construction.cpp
+++ BST_construction.cpp
@@ -1,3 +1,33 @@
+/************  BST deletion ******************************/
+        if (!root) return root;
+        
+        if (val < root->val) {
+            root->left = deleteNode(root->left, val);
+        } else if (val > root->val) {
+            root->right = deleteNode(root->right, val);
+        } else {
+            /* Leaf node case */
+            if (!root->left && !root->right) {
+                delete(root);
+                return NULL;
+            }
+            /* 1 child case */
+            if (!root->left || !root->right) {
+                TreeNode *ret = root->left ? root->left : root->right;
+                delete(root);
+                return ret;
+            }
+            /* 2 child case */
+            if (root->left && root->right) {
+                TreeNode *tmp = root->right;
+                while (tmp->left) {
+                    tmp = tmp->left;
+                }
+                root->val = tmp->val;
+                root->right = deleteNode(root->right, root->val);
+            }
+        }
+        return root;
 /************  BST from preorder traversals **************/ 
 Node* constructTree ( int pre[], int size ){ 
 	stack<Node *> st; 
